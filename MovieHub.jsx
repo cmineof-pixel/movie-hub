91f3eb46a8343456ff419441101a8b7aa49727e0
cmineof-pixel <cmineof@gmail.com>
2026-05-13 00:41:38 +0530

    Add files via upload

---
+++ MovieHub.jsx
@@ -0,0 +1,7 @@
+export default function MovieHub() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-600">MovieHub</h1>
+    </div>
+  );
+}
